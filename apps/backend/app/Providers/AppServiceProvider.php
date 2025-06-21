3d702463c428dfca4bc85a2c32788a53f391ad2c
Lucas Barcelos <lucavm87@gmail.com>
2025-06-21 13:45:05 -0300

    feat: create a Providers

--- apps/backend/app/Providers/AppServiceProvider.php
+++ apps/backend/app/Providers/AppServiceProvider.php
@@ -3,20 +3,19 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Domain\Interfaces\MaterialRepositoryInterface;
+use App\Infrastructure\Persistence\Eloquent\MaterialRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        //
+        $this->app->bind(
+            MaterialRepositoryInterface::class,
+            MaterialRepository::class
+        );
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         //
